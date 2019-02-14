e1437c826ab6cd947f000c460bfeedec9317bb50
Ivan Medvedev <chuchello@gmail.com>
2019-02-15 00:42:42 +0300

    fc

---
+++ precache-manifest.61a598f59fbb889c240ac8967f6e781d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01aef4c5bccafbf86a9c",
+    "url": "/static/css/main.ebe6e210.chunk.css"
+  },
+  {
+    "revision": "01aef4c5bccafbf86a9c",
+    "url": "/static/js/main.01aef4c5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d6232530d2d4d8f07903",
+    "url": "/static/js/2.d6232530.chunk.js"
+  },
+  {
+    "revision": "187735062363e2f8ac396ba7c4b4e77f",
+    "url": "/static/media/icon.18773506.png"
+  },
+  {
+    "revision": "51df1f28474cc6f61c851c6e9f286e0b",
+    "url": "/index.html"
+  }
+];
